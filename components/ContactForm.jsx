72f06c0eb74c24754050d24c2f23cc4e68b401f6
shoaib <shoaibhossain24@gmail.com>
2023-12-15 04:44:16 +0600

    Add dependencies and update ContactForm component

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -1,73 +1,100 @@
-'use client'
-import { useState } from 'react';
+"use client";
 
-const ContactForm = () => {
-  const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
+import { useState } from "react";
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+export default function ContactForm() {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [error, setError] = useState([]);
+  const [success, setSuccess] = useState(false);
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    // Logic to handle form submission (e.g., API call to send data)
-    console.log(formData);
-    // Reset form fields after submission (if needed)
-    setFormData({ name: '', email: '', message: '' });
+
+    console.log("Full name: ", fullname);
+    console.log("Email: ", email);
+    console.log("Message: ", message);
+
+    const res = await fetch("api/contact", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        fullname,
+        email,
+        message,
+      }),
+    });
+
+    const { msg, success } = await res.json();
+    setError(msg);
+    setSuccess(success);
+
+    if (success) {
+      setFullname("");
+      setEmail("");
+      setMessage("");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit} className="max-w-md mx-auto mt-8">
-      <div className="mb-4">
-        <label htmlFor="name" className="block mb-2 font-semibold">Name</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          required
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block mb-2 font-semibold">Email</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-          required
-        />
-      </div>
-      <div className="mb-4">
-        <label htmlFor="message" className="block mb-2 font-semibold">Message</label>
-        <textarea
-          id="message"
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          rows={4}
-          className="w-full px-4 py-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:border-indigo-500"
-          required
-        ></textarea>
-      </div>
-      <div className="text-center">
-        <button
-          type="submit"
-          className="px-6 py-3 bg-indigo-500 text-white rounded-md hover:bg-indigo-600 focus:outline-none"
-        >
-          Submit
-        </button>
-      </div>
-    </form>
-  );
-};
+    <>
+      <form
+        onSubmit={handleSubmit}
+        className="py-4 mt-4 border-t flex flex-col gap-5"
+      >
+        <div>
+          <label htmlFor="fullname">Full Name</label>
+          <input
+            onChange={(e) => setFullname(e.target.value)}
+            value={fullname}
+            type="text"
+            id="fullname"
+            placeholder="John Doe"
+          />
+        </div>
 
-export default ContactForm;
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            type="text"
+            id="email"
+            placeholder="john@gmail.com"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="message">Your Message</label>
+          <textarea
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            className="h-32"
+            id="message"
+            placeholder="Type your message here..."
+          ></textarea>
+        </div>
+
+        <button className="bg-green-700 p-3 text-white font-bold" type="submit">
+          Send
+        </button>
+      </form>
+
+      <div className="bg-slate-100 flex flex-col">
+        {error &&
+          error.map((e) => (
+            <div
+              className={`${
+                success ? "text-green-800" : "text-red-600"
+              } px-5 py-2`}
+            >
+              {e}
+            </div>
+          ))}
+      </div>
+    </>
+  );
+}
